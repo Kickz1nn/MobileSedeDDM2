546dfcc83d86355cefaae90829912aa5ceb44e33
= <=>
2025-08-22 09:03:18 -0300

    a

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [name, setNome] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text
+        style={{ 
+          fontSize: 30, 
+          color: 'blue',
+          fontWeight: 'bold',
+          marginBottom: 20 }}
+      >Nome:</Text> 
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginBottom: 20,
+          paddingHorizontal: 10,
+          width: '80%',
+        }}
+          onChangeText={(value) => setNome(value)}
+        placeholder="Digite seu nome"
+      />
+      <Pressable style={{
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+      }}
+      onPress={() => alert('Nome enviado!')}>
+
+
+        <Text style={{ color: 'white' }}>VER</Text>
+      </Pressable>
+        {
+           name ? <Text style={{marginTop: 20, fontSize: 20}}
+           >Olá, {name}!</Text> : null
+        }
     </View>
   );
 }
